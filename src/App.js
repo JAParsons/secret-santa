87cf699ff1e69fd9a6b22a6ccc82daa7e964008d
JAParsons <johnparsons932@gmail.com>
2020-10-03 19:08:58 +0100

    Scaffolded main page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <img src={"https://via.placeholder.com/150"} />
+        <h1>Secret Santa Generator</h1>
+      </div>
+      <button onClick={() => console.log("Button clicked")}>
+        Generate Secret Santa
+      </button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("Form submitted");
+        }}
+      >
+        <input type="text" placeholder="Enter a name..." />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
